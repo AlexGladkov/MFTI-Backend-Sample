edc3faef31beca23bd62ea663844c7749f9053c7
Alex <bob298@yandex.ru>
2023-02-27 19:52:26 +0700

    added jar task

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,19 @@ tasks.create("stage") {
 }
 
 tasks.withType<Jar> {
+    // Otherwise you'll get a "No main manifest attribute" error
     manifest {
         attributes["Main-Class"] = "tech.mfti.ApplicationKt"
     }
+
+    // To avoid the duplicate handling strategy error
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    // To add all of the dependencies otherwise a "NoClassDefFoundError" error
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
 }
